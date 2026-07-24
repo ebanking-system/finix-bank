d3591523b142aa0f3d81aa17482a41266cde52cc
sandesh007 <sandeshwaingade3999@gmail.com>
2026-07-24 22:46:23 +0530

    Third Commit -> KYC Entities created

---
+++ backend/banking-service/src/main/java/com/finix/kyc/entity/KycDocuments.java
@@ -0,0 +1,47 @@
+package com.finix.kyc.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CurrentTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NegativeOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name = "kyc_documents")
+public class KycDocuments {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	// customerId should be here 
+	// + 
+	// specify association relationship here
+	@Column(name="aadhar_number", nullable = false, unique = true)
+	private String aadharNum;
+	@Column(name="pan_num", nullable = false, unique= true)
+	private String panNum;
+	@Column(name="self_image", nullable = false)
+	private String selfImage;
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	@CurrentTimestamp
+	@Column(name="submitted_date")
+	private LocalDateTime submittedDate;
+}
